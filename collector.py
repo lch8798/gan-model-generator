fab8454eefaf6a5a65af3d0592cfe50b55a11c7e
onCloud241 <lch8798@gmail.com>
2023-03-21 21:05:09 +0900

    fix. chorme option add secret mode

--- collector.py
+++ collector.py
@@ -23,6 +23,7 @@ account = {
 def get_chrome_driver():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--incognito')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
 
